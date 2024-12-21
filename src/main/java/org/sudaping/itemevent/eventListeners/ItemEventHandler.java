2d0f071e095b31b02a3a270b1e41a091ec264b74
topen0330 <98753980+paperntman@users.noreply.github.com>
2024-12-22 00:41:51 +0900

    added prefix

--- src/main/java/org/sudaping/itemevent/eventListeners/ItemEventHandler.java
+++ src/main/java/org/sudaping/itemevent/eventListeners/ItemEventHandler.java
@@ -1,9 +1,13 @@
 package org.sudaping.itemevent.eventListeners;
 
+import net.kyori.adventure.text.Component;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.sudaping.itemevent.ItemEventListener;
 import org.sudaping.itemevent.commands.Registration;
 import org.sudaping.itemevent.itemListeners.Fly5m;
@@ -24,6 +28,7 @@ public class ItemEventHandler implements Listener {
 
     @EventHandler
     public void onInteraction(PlayerInteractEvent e){
+        if (e.getHand() == null || e.getHand().equals(EquipmentSlot.OFF_HAND) || e.getAction().equals(Action.RIGHT_CLICK_AIR)) return;
         ItemStack item = e.getPlayer().getInventory().getItemInMainHand();
         if (item.getItemMeta() == null) return;
 
@@ -32,7 +37,11 @@ public class ItemEventHandler implements Listener {
             String t_key = itemStackStringEntry.getValue();
             String key = null;
 
-            if (t_item.getItemMeta().displayName().equals(item.getItemMeta().displayName()))
+            ItemMeta itemMeta = t_item.getItemMeta();
+            if (itemMeta == null) continue;
+            Component component = itemMeta.displayName();
+            if (component == null) continue;
+            if (component.equals(item.getItemMeta().displayName()))
                 key = t_key;
 
             if(key == null) continue;
